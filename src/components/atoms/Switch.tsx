087daa56f220869e5602c9253b76035cfad4e532
Hector OG <59241967+CI5KO@users.noreply.github.com>
2025-12-29 11:47:22 -0600

    fix/header

--- src/components/atoms/Switch.tsx
+++ src/components/atoms/Switch.tsx
@@ -21,14 +21,14 @@ export default function Switch({
       }}
       className={`relative w-16 h-10 border rounded-lg transition-colors duration-300 cursor-pointer overflow-hidden ${
         isOn
-          ? "bg-complementary border-pink-500 dark:border-pink-700 dark:bg-complementary-dark"
+          ? "bg-complementary border-white dark:bg-complementary-dark dark:border-black"
           : "bg-gray-300 border-gray-500 dark:border-gray-700 dark:bg-gray-600"
       }`}
     >
       <div className="relative w-full h-full flex items-center">
         <div
           className={`absolute w-6 h-6 transition-transform duration-300 ease-in-out ${
-            isOn ? "translate-x-5 sm:translate-x-7" : "translate-x-1"
+            isOn ? "translate-x-8" : "translate-x-1"
           }`}
         >
           {isOn ? IconOn : IconOff}
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import Image from "next/image";
 import ThemeSwitcher from "../molecules/ThemeSwitcher";
+
+import AppLogo from "../../../public/logo.png";
 
 export default function Header() {
   return (
-    <div className="w-full bg-complementary dark:bg-complementary-dark">
+    <header className="w-full py-1 flex flex-row items-center justify-between px-4 bg-gray-100 dark:bg-gray-900 shadow">
+      <Image src={AppLogo.src} alt="Logo" width={40} height={40} />
       <ThemeSwitcher />
-    </div>
+    </header>
   );
 }
